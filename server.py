139538f4ba5959b4f6df85882df987fbb7d097b1
Jacky Chen <jc4283@drexel.edu>
2022-08-09 22:35:19 -0400

    added temp. logo for going back to landing page and refactored css files

--- server.py
+++ server.py
@@ -26,5 +26,9 @@ def login():
 def register():
     return render_template("register.html")
 
+@app.route('/index.html')
+def index():
+    return render_template("index.html")
+
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8080, debug=True)
